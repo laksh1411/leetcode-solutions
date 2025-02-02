0e121504520b765b4ad8f4b4ccf3e438aec2817e
laksh1411 <lakshsaluja2@gmail.com>
2025-02-02 17:09:08 +0000

    Sync LeetCode submission Runtime - 45 ms (27.05%), Memory - 44.8 MB (99.98%)

---
+++ 0001-two-sum/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] n, int target) {
+        int sum=0,k=0;
+        int arr[]=new int[2];
+        for(int i=0;i<n.length;i++){
+            sum=0;
+            for(int j=i+1;j<n.length;j++){
+                sum=n[i]+n[j];
+                if(sum==target){
+                    arr[k]=i;
+                    arr[++k]=j;
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+}
