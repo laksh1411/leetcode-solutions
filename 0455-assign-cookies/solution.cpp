56cc0f11dc66da9ca893872535a38aef06809e6e
laksh1411 <lakshsaluja2@gmail.com>
2025-11-05 17:26:49 +0000

    Sync LeetCode submission Runtime - 7 ms (71.48%), Memory - 44.9 MB (47.87%)

---
+++ 0455-assign-cookies/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int a=0,b=0,count=0;
+        while(a<g.size()&&b<s.size()){
+            if(g[a]<=s[b]){
+                a++;b++;count++;
+                //break;
+            }
+            else b++;
+        }
+        return count;
+    }
+};
