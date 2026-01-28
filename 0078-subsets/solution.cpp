14270ec409c6addf0612cb167bea0fa6d53c2ef5
laksh1411 <lakshsaluja2@gmail.com>
2026-01-28 04:38:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (60.54%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void getsubsets(vector<int>& nums,vector<int>& arr,int i,vector<vector<int>>& allsubs){
-        if(i==nums.size()){
-            allsubs.push_back({arr});
+    void getsubs(vector<int>& nums,int index,vector<int>& arr,vector<vector<int>> &ans){
+        if(index==nums.size()){
+            ans.push_back({arr});
             return;
         }
-        arr.push_back(nums[i]);
-        getsubsets(nums,arr,i+1,allsubs);
-
+        arr.push_back(nums[index]);
+        getsubs(nums,index+1,arr,ans);
         arr.pop_back();
-        getsubsets(nums,arr,i+1,allsubs);
+        getsubs(nums,index+1,arr,ans);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> allsubs;
+        vector<vector<int>> ans;
         vector<int> arr;
-        getsubsets(nums,arr,0,allsubs);
-        return allsubs;
+        getsubs(nums,0,arr,ans);
+        return ans;
     }
 };
