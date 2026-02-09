03443db1b9ae93ef7f6ebf98dc44a4780f740c8e
laksh1411 <lakshsaluja2@gmail.com>
2026-02-09 17:00:01 +0000

    Sync LeetCode submission Runtime - 16 ms (84.98%), Memory - 139.3 MB (43.94%)

---
+++ 0239-sliding-window-maximum/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> idx;
+        vector<int> ans;
+
+        for(int i=0;i<nums.size();i++){
+            if(!idx.empty() && idx.front()<=i-k ) idx.pop_front();
+
+            while(!idx.empty() && nums[idx.back()]<nums[i]){
+                idx.pop_back();
+            }
+            idx.push_back(i);
+            if(i>=k-1){
+                ans.push_back(nums[idx.front()]);
+            }
+        }
+        return ans;
+    }
+};
