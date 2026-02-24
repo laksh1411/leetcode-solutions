0019b203a3b08ad8d4988268ac246e7cfad7d83f
laksh1411 <lakshsaluja2@gmail.com>
2026-02-24 07:27:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22 MB (45.20%)

--- 0098-validate-binary-search-tree/solution.cpp
+++ 0098-validate-binary-search-tree/solution.cpp
@@ -11,20 +11,13 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
+    bool check(TreeNode* root,long min,long max){
+        if(!root) return true;
+        if(root->val<=min || root->val>=max) return false;
+        return check(root->left,min,root->val) && check(root->right,root->val,max);
     }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return false;
-        vector<int> ans;
-        inorder(root,ans);
-
-        for(int i=0;i<ans.size()-1;i++){
-            if(ans[i]>=ans[i+1]) return false;
-        }
-        return true;
+        return check(root,LLONG_MIN,LLONG_MAX);
     }
 };
+
